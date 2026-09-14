17cf03992aa58e58ea3784cbbd5c1d3583acfa09
Jose RF Junior <web2ajax@gmail.com>
2026-09-14 16:11:53 -0300

    GPT-5.6 Sol: remove dead duplicate identity component

--- js/components/GovBar.js
+++
@@ -1,1 +0,0 @@
-export const GovBar={render(){return `<div class="govbar"><b>HERACLITUSDB</b><span class="dot"></span><span>Plataforma de dados temporais, proveniência e evidência verificável</span></div>`}};
